7cdc0e80d940722097511e6304f9c15b94a34de6
TekCode <est.felipe.aponte@unimilitar.edu.co>
2024-01-22 15:22:05 -0500

    Register form added

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
+
+def register(request):
+    return render(request, 'main/register.html')
     
