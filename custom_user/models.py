838f7f6ea9420c19036d2406d656811ccabe4c86
TekCode <est.felipe.aponte@unimilitar.edu.co>
2024-01-31 15:13:18 -0500

    Added user registration

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,5 +1,21 @@
 from django_use_email_as_username.models import BaseUser, BaseUserManager
+from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class User(BaseUser):
     objects = BaseUserManager()
+
+class userData(models.Model):
+    user_model = get_user_model()
+
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE, default=None)
+    user_firstname = models.CharField(max_length=200, blank=True)
+    user_lastname = models.CharField(max_length=200, blank=True)
+    university = models.CharField(max_length=200, blank=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user_firstname.capitalize() + ' ' + self.user_lastname.capitalize()
+    
+
--- main/views.py
+++ main/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from custom_user.models import userData
 
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
 
+def loginPage(request):
+	context = {}
+	if request.method == 'POST':
+		username = request.POST.get('username').lower()
+		password = request.POST.get('password')
+
+		try:
+			user = User.objects.get(username=username)
+		except:
+			pass
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+			
+		else:
+			pass
+
+	return render(request, 'main/login_register.html', context)
+
 def register(request):
+    if request.method == 'POST':
+        #print('asd')
+        firstname = request.POST.get('username').lower()
+        lastname = request.POST.get('apellidos').lower()
+        email = request.POST.get('email')
+        universidad = request.POST.get('universidades')
+        password = request.POST.get('password')
+
+        User = get_user_model()
+        user = User.objects.create_user(email=email,
+                                 		password=password)
+        
+        new_user = userData(user = user, 
+                            user_firstname = firstname, 
+                            user_lastname = lastname,
+                            university = universidad, 
+                            email = email, )
+        
+        new_user.save()
+
+        return render(request, 'main/index.html')
+
     return render(request, 'main/register.html')
     
